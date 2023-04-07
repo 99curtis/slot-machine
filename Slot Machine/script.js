64e330edcc3078052bf7fbae8374b5ad42933a9a
Curtis Officer <officercd@yahoo.com>
2023-04-07 14:41:36 -0500

    2nd commit

--- Slot Machine/script.js
+++ Slot Machine/script.js
@@ -4,150 +4,150 @@ const status = document.getElementById('status');
 const slots = document.querySelectorAll('.slot');
 const spinBtn = document.getElementById('spin');
 const betButtons = document.querySelectorAll('.bet-buttons button');
-const payout7NotFirstTwoCell = document.getElementById("payout_7_not_first_two");
-const payout17Cell = document.getElementById("payout_1_7");
-const payout2EqualCell = document.getElementById("payout_2_equal");
-const resultMessage = document.getElementById("resultMessage");
+const resultMessage = document.getElementById('resultMessage');
 
 let balance = 0;
 let spinning = false;
 let selectedBet = null;
-const payoutHeader = document.getElementById('payoutHeader');
-const jackpotCell = document.getElementById('jackpot');
+const baseBet = 0.08;
+const betAmounts = [baseBet, baseBet * 2, baseBet * 3, baseBet * 4, baseBet * 5];
 
 betButtons.forEach((button, index) => {
   button.addEventListener("click", () => {
     if (spinning) return;
 
-    // Clear the active state from other buttons
     betButtons.forEach((btn) => btn.classList.remove("active"));
-
-    // Set the clicked button as active
     button.classList.add("active");
 
-    selectedBet = index + 1;
-
-    payoutHeader.style.opacity = 0;
-    setTimeout(() => {
-      jackpotCell.textContent = `Jackpot (${selectedBet * 100})`;
-      payout7NotFirstTwoCell.textContent = `${selectedBet * 5}`;
-      payout17Cell.textContent = `${selectedBet * 2}`;
-      payout2EqualCell.textContent = `${selectedBet}`;
-      payoutHeader.style.opacity = 1;
-    }, 300);
+    selectedBet = betAmounts[index];
   });
+});
+
+spinBtn.addEventListener('click', async () => {
+  if (spinning || selectedBet === null) return;
+
+  if (selectedBet > balance) {
+    alert("You don't have enough balance.");
+    return;
+  }
+
+  balance -= selectedBet;
+  spinning = true;
+  status.textContent = `Spinning...`;
+
+  const results = Array.from(slots).map(() => Math.floor(Math.random() * 9) + 1);
+  await spinSlots(results);
+  highlightResults(results);
+
+  spinning = false;
+  status.textContent = `Balance: ${balance.toFixed(2)}`;
+
+  const payout = calculatePayout(results);
+  balance += payout;
+
+  if (payout > 0) {
+    resultMessage.textContent = `You won $${payout.toFixed(2)} with the combination ${results.join('-')}`;
+  } else {
+    resultMessage.textContent = `Sorry, you didn't win this time.`;
+  }
+});
+
+addMoneyBtn.addEventListener('click', () => {
+  const money = parseFloat(moneyInput.value);
+  if (isNaN(money) || money <= 0) {
+    alert('Please enter a valid amount.');
+    return;
+  }
+  balance += money;
+  status.textContent = `Balance: ${balance.toFixed(2)}`;
 });
 
 async function spinSlots(results) {
   for (let i = 0; i < slots.length; i++) {
     const slot = slots[i];
-    const inner = slot.querySelector(".slot-inner");
-    inner.textContent = '';
+    const finalNumber = results[i];
 
-    const spinDuration = (i > 0 && results.slice(0, i).every((r) => r === 9)) ? 2000 : 1000;
-
-    if (i > 0 && results.slice(0, i).every((r) => r === 9)) {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+    for (let j = 0; j < 10; j++) {
+      slot.textContent = (j % 9) + 1;
+      slot.style.backgroundColor = '';
+      await sleep(100);
     }
 
-    await new Promise((resolve) => {
-      const translateYValue = window.innerWidth > 600 ? -50 : -35;
-      const animation = inner.animate(
-        [
-          { transform: `translateY(${translateYValue}%)` },
-          { transform: `translateY(-${results[i] * 110 + translateYValue}%)` },
-        ],
-        { duration: spinDuration, easing: "ease-out", fill: "forwards" }
-      );
-
-      animation.onfinish = () => {
-        inner.style.transform = `translateY(-${results[i] * 110 + translateYValue}%)`;
-        inner.textContent = results[i];
-        resolve();
-      };
-    });
+    slot.textContent = finalNumber;
   }
-
-  spinBtn.addEventListener('click', async () => {
-    if (spinning || selectedBet === null) return;
-
-    if (selectedBet > balance) {
-      alert("You don't have enough balance.");
-      return;
-    }
-
-    balance -= selectedBet;
-    spinning = true;
-
-    const results = Array.from(slots).map(() => Math.floor(Math.random() * 9) + 1);
-    await spinSlots(results);
-    spinning = false;
-
-    const payout = calculatePayout(results);
-    balance += payout;
-
-    status.textContent = `Balance: ${balance.toFixed(2)}`;
-
-    if (payout > 0) {
-      resultMessage.textContent = `You won ${payout.toFixed(2)} with the combination ${results.join('-')}`;
-    } else {
-      resultMessage.textContent = `Sorry, you didn't win this time.`;
-    }
-  });
-
-  addMoneyBtn.addEventListener('click', () => {
-    const money = parseFloat(moneyInput.value);
-    if (isNaN(money) || money <= 0) {
-      alert('Please enter a valid amount.');
-      return;
-    }
-    balance += money;
-    status.textContent = `Balance: ${balance.toFixed(2)}`;
-  });
-  
-
-  // Highlight boxes depending on the outcome
-  const payout = calculatePayout(results);
-  slots.forEach((slot, index) => {
-    slot.style.borderColor = payout > 0 ? "green" : "red";
-  });
-
-  return payout;
 }
 
-window.addEventListener("resize", () => {
-    const translateYValue = window.innerWidth > 600 ? -50 : -35;
-    slots.forEach((slot, index) => {
-      const inner = slot.querySelector(".slot-inner");
-      if (inner.textContent) {
-        inner.style.transform = `translateY(-${parseInt(inner.textContent) * 110 + translateYValue}%)`;
-      }
-    });
+function highlightResults(results) {
+  const winIndices = [];
+  const loseIndices = [];
+
+  // Code to determine winning and losing indices
+
+  winIndices.forEach(index => {
+    slots[index].style.backgroundColor = 'limegreen';
   });
-  
-  function calculatePayout(results) {
-    const uniqueNumbers = new Set(results);
-    const count = results.reduce((acc, curr) => {
-      acc[curr] = (acc[curr] || 0) + 1;
-      return acc;
-    }, {});
-  
-    if (uniqueNumbers.size === 1) {
-      if (results[0] === 9) {
-        return selectedBet * 100;
-      } else if (results[0] === 7) {
-        return selectedBet * 20;
+  loseIndices.forEach(index => {
+    slots[index].style.backgroundColor = 'red';
+  });
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function calculatePayout(results) {
+  const winIndices = [];
+  const loseIndices = [];
+
+  if (results.every(num => num === 7)) {
+    winIndices.push(0, 1, 2, 3, 4);
+  } else if (results.slice(0, 4).every(num => num === 7)) {
+    winIndices.push(0, 1, 2, 3);
+    loseIndices.push(4);
+  } else if (results.slice(0, 3).every(num => num === 7)) {
+    winIndices.push(0, 1, 2);
+    loseIndices.push(3, 4);
+  } else if (results.slice(0, 2).every(num => num === 7)) {
+    winIndices.push(0, 1);
+    loseIndices.push(2, 3, 4);
+  } else if (results[0] === 7) {
+    winIndices.push(0);
+    loseIndices.push(1, 2, 3, 4);
+  } else {
+    for (let i = 0; i < 4; i++) {
+      if (results[i] === results[i + 1]) {
+        winIndices.push(i, i + 1);
       } else {
-        return selectedBet * 10;
+        loseIndices.push(i);
       }
-    } else if (uniqueNumbers.size === 2 && count[7] === 2) {
-      return selectedBet * 5;
-    } else if (count[7] === 1) {
-      return selectedBet * 2;
-    } else if (uniqueNumbers.size === 2) {
-      return selectedBet;
-    } else {
-      return Math.floor(Math.random() * 50) / 100;
     }
+    loseIndices.push(4);
   }
-  
+
+  winIndices.forEach(index => {
+    slots[index].style.backgroundColor = 'limegreen';
+  });
+  loseIndices.forEach(index => {
+    slots[index].style.backgroundColor = 'red';
+  });
+
+  if (results.every(num => num === 7)) {
+    return selectedBet * 1000;
+  } else if (results.slice(0, 4).every(num => num === 7)) {
+    return selectedBet * 100;
+  } else if (results.slice(0, 3).every(num => num === 7)) {
+    return selectedBet * 50;
+  } else if (results.slice(0, 2).every(num => num === 7)) {
+    return selectedBet * 20;
+  } else if (results[0] === 7) {
+    return selectedBet * 5;
+  } else if (results.slice(0, 3).every((num, idx) => num === results[idx + 1])) {
+    return selectedBet * 10;
+  } else if (results.slice(0, 2).every((num, idx) => num === results[idx + 1])) {
+    return selectedBet * 5;
+  } else if (results[0] === results[1] || results[1] === results[2] || results[2] === results[3] || results[3] === results[4]) {
+    return selectedBet * 2;
+  } else {
+    return 0;
+    }
+}
